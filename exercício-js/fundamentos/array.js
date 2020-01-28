a57e43b4765f22d192c358235e9c5e2803b17bc8
yasminsouzadev <yasminsouza.dev@gmail.com>
2020-01-28 18:57:59 -0300

    first commit

---
+++ exercício-js/fundamentos/array.js
@@ -0,0 +1,9 @@
+const valores = [7.7, 8.9, 6.3, 9.2]
+console.log(valores[0], valores [3])
+console.log(valores[4])
+
+valores[4] = 10
+console.log(valores)
+console.log(valores.length)
+
+valores.push({id: 3}, false, null, 'teste')
---
+++ exercício-js/fundamentos/comentarios.js
@@ -0,0 +1,12 @@
+// Comentário de uma Linha
+console.log('Linha 1')
+/*
+Comentário de 
+múltiplas linhas 
+*/
+console.log('Linha 2')
+
+/*
+*comentário de 
+*múltiplas Linhas
+*/
